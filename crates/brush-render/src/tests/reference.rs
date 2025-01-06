2ddc58348c78f611fdd15d8ab166cfff86cb23ab
Arthur Brussee <arthur.brussee@gmail.com>
2025-01-06 17:53:01 +0000

    Revert precision

--- crates/brush-render/src/tests/reference.rs
+++ crates/brush-render/src/tests/reference.rs
@@ -114,7 +114,7 @@ async fn test_reference() -> Result<()> {
         wrapped_aux.clone().debug_assert_valid();
 
         // Check if images match.
-        assert!(out.clone().all_close(img_ref, Some(1e-4), Some(1e-5)));
+        assert!(out.clone().all_close(img_ref, Some(1e-5), Some(1e-6)));
 
         let num_visible = wrapped_aux.num_visible.into_scalar_async().await as usize;
         let projected_splats =
