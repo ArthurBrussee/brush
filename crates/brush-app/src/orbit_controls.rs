708df4d1c3400f1206e84d4c2c9717d08509d83c
Arthur <arthur.brussee@gmail.com>
2025-01-23 15:33:47 +0000

    Momenta

--- crates/brush-app/src/orbit_controls.rs
+++ crates/brush-app/src/orbit_controls.rs
@@ -1,14 +1,15 @@
 use core::f32;
 
 use egui::Response;
-use glam::{Quat, Vec3};
+use glam::{Quat, Vec2, Vec3};
 
 pub struct CameraController {
     pub position: Vec3,
     pub rotation: Quat,
-
     roll: Quat,
     focus_distance: f32,
+    fly_velocity: Vec3,
+    orbit_velocity: Vec2,
 }
 
 pub fn smooth_orbit(
@@ -35,6 +36,16 @@ pub fn smooth_orbit(
     (new_position, new_rotation)
 }
 
+fn exp_lerp2(a: Vec2, b: Vec2, dt: f32, lambda: f32) -> Vec2 {
+    let lerp_exp = (-lambda * dt).exp();
+    a * lerp_exp + b * (1.0 - lerp_exp)
+}
+
+fn exp_lerp3(a: Vec3, b: Vec3, dt: f32, lambda: f32) -> Vec3 {
+    let lerp_exp = (-lambda * dt).exp();
+    a * lerp_exp + b * (1.0 - lerp_exp)
+}
+
 impl CameraController {
     pub fn new(start_focus_distance: f32) -> Self {
         Self {
@@ -42,88 +53,149 @@ impl CameraController {
             rotation: Quat::IDENTITY,
             roll: Quat::IDENTITY,
             focus_distance: start_focus_distance,
+            fly_velocity: Vec3::ZERO,
+            orbit_velocity: Vec2::ZERO,
         }
     }
 
     pub fn tick(&mut self, response: &Response, ui: &egui::Ui) {
-        let move_speed = 5.0
-            * ui.input(|r| r.predicted_dt)
+        let delta_time = ui.input(|r| r.predicted_dt);
+
+        let lmb = response.dragged_by(egui::PointerButton::Primary);
+        let rmb = response.dragged_by(egui::PointerButton::Secondary);
+        let mmb = response.dragged_by(egui::PointerButton::Middle);
+
+        let look_pan = mmb || lmb && ui.input(|r| r.modifiers.ctrl);
+        let look_fps = rmb || lmb && ui.input(|r| r.key_down(egui::Key::Space));
+        let look_orbit = lmb;
+
+        let mouselook_speed = 0.002;
+
+        let right = self.rotation * Vec3::X;
+        let up = self.rotation * Vec3::Y;
+        let forward = self.rotation * Vec3::Z;
+
+        if response.hovered() {
+            if ui.input(|r| r.modifiers.ctrl) {
+                ui.ctx().set_cursor_icon(egui::CursorIcon::Move);
+            } else if ui.input(|r| r.key_down(egui::Key::Space)) {
+                ui.ctx().set_cursor_icon(egui::CursorIcon::Crosshair);
+            } else {
+                ui.ctx().set_cursor_icon(egui::CursorIcon::PointingHand);
+            }
+        }
+
+        if look_pan {
+            let drag_mult = self.focus_distance / response.rect.width().max(response.rect.height());
+            self.position -= right * response.drag_delta().x * drag_mult;
+            self.position -= up * response.drag_delta().y * drag_mult;
+
+            ui.ctx().set_cursor_icon(egui::CursorIcon::Move);
+        } else if look_fps {
+            let axis = response.drag_delta();
+            let yaw = Quat::from_axis_angle(self.roll * Vec3::Y, axis.x * mouselook_speed);
+            let pitch = Quat::from_rotation_x(-axis.y * mouselook_speed);
+            self.rotation = yaw * self.rotation * pitch;
+            ui.ctx().set_cursor_icon(egui::CursorIcon::Crosshair);
+        } else if look_orbit {
+            let dx = response.drag_delta().x * mouselook_speed;
+            let dy = -response.drag_delta().y * mouselook_speed;
+
+            self.orbit_velocity = glam::vec2(dy, dx);
+            ui.ctx().set_cursor_icon(egui::CursorIcon::PointingHand);
+        }
+
+        (self.position, self.rotation) = smooth_orbit(
+            self.position,
+            self.rotation,
+            self.roll,
+            self.orbit_velocity.x,
+            self.orbit_velocity.y,
+            self.focus_distance,
+        );
+
+        let fly_moment_lambda = 0.8;
+
+        let move_speed = 30.0
             * if ui.input(|r| r.modifiers.shift) {
                 4.0
             } else {
                 1.0
             };
 
-        let lmb = response.dragged_by(egui::PointerButton::Primary);
-
-        let is_panning = lmb && ui.input(|r| r.key_down(egui::Key::Space));
-        let is_orbiting = lmb && ui.input(|r| r.modifiers.alt);
-        let is_flythrough = lmb && !is_panning && !is_orbiting;
-
-        let mouselook_speed = 0.0025;
-
-        let right = self.rotation * Vec3::X;
-        let up = self.rotation * Vec3::Y;
-        let forward = self.rotation * Vec3::Z;
-
-        if is_orbiting {
-            let dx = response.drag_delta().x * mouselook_speed;
-            let dy = -response.drag_delta().y * mouselook_speed;
-
-            (self.position, self.rotation) = smooth_orbit(
-                self.position,
-                self.rotation,
-                self.roll,
-                dy,
-                dx,
-                self.focus_distance,
-            );
-        } else if is_panning {
-            let drag_mult = 0.1;
-            self.position -= right * response.drag_delta().x * drag_mult * move_speed;
-            self.position -= up * response.drag_delta().y * drag_mult * move_speed;
-        } else if is_flythrough {
-            let axis = response.drag_delta();
-            let yaw = Quat::from_axis_angle(self.roll * Vec3::Y, axis.x * mouselook_speed);
-            let pitch = Quat::from_rotation_x(-axis.y * mouselook_speed);
-            self.rotation = yaw * self.rotation * pitch;
-        }
-
-        // In Unity, this is only enabled when the camera is in flythrough mode.
-        // for our purposes... just enable it.
         if ui.input(|r| r.key_down(egui::Key::W) || r.key_down(egui::Key::ArrowUp)) {
-            self.position += forward * move_speed;
+            self.fly_velocity = exp_lerp3(
+                self.fly_velocity,
+                Vec3::Z * move_speed,
+                delta_time,
+                fly_moment_lambda,
+            );
         }
         if ui.input(|r| r.key_down(egui::Key::A) || r.key_down(egui::Key::ArrowLeft)) {
-            self.position -= right * move_speed;
+            self.fly_velocity = exp_lerp3(
+                self.fly_velocity,
+                -Vec3::X * move_speed,
+                delta_time,
+                fly_moment_lambda,
+            );
         }
         if ui.input(|r| r.key_down(egui::Key::S) || r.key_down(egui::Key::ArrowDown)) {
-            self.position -= forward * move_speed;
+            self.fly_velocity = exp_lerp3(
+                self.fly_velocity,
+                -Vec3::Z * move_speed,
+                delta_time,
+                fly_moment_lambda,
+            );
         }
         if ui.input(|r| r.key_down(egui::Key::D) || r.key_down(egui::Key::ArrowRight)) {
-            self.position += right * move_speed;
+            self.fly_velocity = exp_lerp3(
+                self.fly_velocity,
+                Vec3::X * move_speed,
+                delta_time,
+                fly_moment_lambda,
+            );
         }
 
-        // Move _down_ with Q
-        if ui.input(|r| r.key_down(egui::Key::Q)) {
-            self.position += up * move_speed;
-        }
-        // Move up with E
-        if ui.input(|r| r.key_down(egui::Key::E)) {
-            self.position -= up * move_speed;
+        if ui.input(|r| r.modifiers.alt) {
+            // Roll with alt + Q&E.
+            if ui.input(|r| r.key_down(egui::Key::Q)) {
+                let roll = Quat::from_axis_angle(forward, move_speed * 0.025 * delta_time);
+                self.rotation = roll * self.rotation;
+                self.roll = roll * self.roll;
+            }
+            if ui.input(|r| r.key_down(egui::Key::E)) {
+                let roll = Quat::from_axis_angle(forward, -move_speed * 0.025 * delta_time);
+                self.rotation = roll * self.rotation;
+                self.roll = roll * self.roll;
+            }
+        } else {
+            // Move _down_ with Q
+            if ui.input(|r| r.key_down(egui::Key::Q)) {
+                self.fly_velocity = exp_lerp3(
+                    self.fly_velocity,
+                    Vec3::Y * move_speed,
+                    delta_time,
+                    fly_moment_lambda,
+                );
+            }
+            // Move up with E
+            if ui.input(|r| r.key_down(egui::Key::E)) {
+                self.fly_velocity = exp_lerp3(
+                    self.fly_velocity,
+                    -Vec3::Y * move_speed,
+                    delta_time,
+                    fly_moment_lambda,
+                );
+            }
         }
 
-        // Roll with alt + Q&E.
-        if ui.input(|r| r.key_down(egui::Key::R)) {
-            let roll = Quat::from_axis_angle(forward, move_speed * 0.05);
-            self.rotation = roll * self.rotation;
-            self.roll = roll * self.roll;
-        }
-        if ui.input(|r| r.key_down(egui::Key::T)) {
-            let roll = Quat::from_axis_angle(forward, -move_speed * 0.05);
-            self.rotation = roll * self.rotation;
-            self.roll = roll * self.roll;
-        }
+        let delta = self.fly_velocity * delta_time;
+        self.position += delta.x * right + delta.y * up + delta.z * forward;
+
+        // Damp velocities towards zero.
+        self.orbit_velocity = exp_lerp2(self.orbit_velocity, Vec2::ZERO, delta_time, 8.0);
+        self.fly_velocity = exp_lerp3(self.fly_velocity, Vec3::ZERO, delta_time, 7.0);
+
         // Handle scroll wheel: move back, and adjust focus distance.
         let scrolled = ui.input(|r| r.smooth_scroll_delta.y);
         let scroll_speed = 0.001;
