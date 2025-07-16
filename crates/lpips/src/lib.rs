235b8989e0f73042086c29bf0e8abcafe321758c
Arthur Brussee <arthur.brussee@gmail.com>
2025-07-16 19:15:13 +0100

    Move to convert project

--- crates/lpips/src/lib.rs
+++ crates/lpips/src/lib.rs
@@ -5,7 +5,6 @@ use burn::nn::conv::Conv2d;
 use burn::nn::conv::Conv2dConfig;
 use burn::nn::pool::MaxPool2d;
 use burn::nn::pool::MaxPool2dConfig;
-use burn::record::FullPrecisionSettings;
 use burn::record::HalfPrecisionSettings;
 use burn::record::NamedMpkGzFileRecorder;
 use burn::record::Recorder;
@@ -161,22 +160,6 @@ impl LpipsModelConfig {
             max_pool: MaxPool2dConfig::new([2, 2]).with_strides([2, 2]).init(),
         }
     }
-}
-
-pub fn convert_lpips<B: Backend>(device: &B::Device) {
-    let model = LpipsModelConfig::new().init::<B>(device);
-    let record: <LpipsModel<B> as Module<B>>::Record =
-        burn_import::pytorch::PyTorchFileRecorder::<FullPrecisionSettings>::default()
-            .load(
-                burn_import::pytorch::LoadArgs::new("./lpips_vgg_remapped.pth".into()),
-                device,
-            )
-            .expect("Should decode state successfully");
-    let model = model.load_record(record);
-    let recorder = burn::record::NamedMpkGzFileRecorder::<HalfPrecisionSettings>::new();
-    model
-        .save_file("./burn_mapped", &recorder)
-        .expect("Failed to convert model");
 }
 
 pub fn load_vgg_lpips<B: Backend>(device: &B::Device) -> LpipsModel<B> {
