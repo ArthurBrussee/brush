356f1fe8199ab5202d46e043a4c63c11f1f44c26
Arthur Brussee <arthur.brussee@gmail.com>
2025-12-15 10:49:53 +0000

    -Os halves the size!

--- crates/brush-wasm/src/lib.rs
+++ crates/brush-wasm/src/lib.rs
@@ -16,7 +16,9 @@ mod three;
 
 pub fn wasm_app(canvas_name: &str) -> anyhow::Result<Arc<UiProcess>> {
     // TODO: Make sure some startup bits here only happen once.
+    #[cfg(debug_assertions)]
     wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+
     let context = Arc::new(UiProcess::new());
 
     let wgpu_options = brush_ui::create_egui_options();
